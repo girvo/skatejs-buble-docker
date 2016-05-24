06e0c6441327189db90414422acd63691a453dc6
Josh Girvin <josh@jgirvin.com>
2016-05-25 00:25:35 +1000

    Showing how to leverage properties internally, for "models"

--- code/client/app.js
+++ code/client/app.js
@@ -3,24 +3,21 @@
  */
 'use strict'
 
+// Base dependencies
 const polyfill = require('./polyfill')
 const skate = require('skatejs')
-const render = require('skatejs-dom-diff').render
-const el = require('skatejs-dom-diff').vdom.element
+
+// vdom-based rendering helpers
+const {render} = require('skatejs-dom-diff')
+const {div} = require('skatejs-dom-diff').vdom.element
 
 // Setup our web component definitions
 skate('hello-world', {
     properties: {
         name: {
-            attribute: true,
-            required: true,
-            // set: skate.render,
+            default: 'Josh',
+            set: skate.render,
         }
-    },
-
-    // XXX: Is this the correct way? Or should we use property[x].set instead?
-    attribute(ele, data) {
-        skate.render(ele)
     },
 
     created(ele) {
@@ -30,6 +27,6 @@ skate('hello-world', {
     },
 
     render: render((ele) => {
-        return el('div', `Hello, ${ele.name}!`)
+        return div(`Hello, ${ele.name}!`)
     })
 })
